a2e016ca697b36ee6d200a68b457941356a19dac
Shubhankar Misra <shubhankar.misra@99acres.com>
2022-09-12 17:50:55 +0530

    saving work

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,6 +3,11 @@ var span = document.getElementsByClassName("close")[0];
 const modalContent = document.getElementById('modal-content');
 
 const openEditor = () => {
+  modalContent.innerHTML = '';
+
+  let dragStartX = '';
+  let dragStartY = '';
+
   const { src, roomName, tourName, } = window.currentImage;
   const h1 = document.createElement('h1');
   h1.innerHTML = `Tour name: ${tourName}`;
@@ -10,17 +15,34 @@ const openEditor = () => {
   const h3 = document.createElement('h3');
   h3.innerHTML = `Room name: ${roomName}`;
 
-  const img = document.createElement('img');
-  img.src = src;
-  img.width = '400';
-  img.height = '400';
+  const img = document.createElement('div');
+  img.style.background = `url(${src}) no-repeat`;
+  img.style.backgroundSize = 'contain';
+  img.style.height = '400px';
+  img.style.position = 'relative';
 
   const hotspotWrapper = document.createElement('div');
-  const hotspot = document.createElement('div');
-  const hotspotHeading = document.createElement('h5');
 
-  hotspotHeading.innerHTML = `Drag this hotspot to connect to another/ same room`
+  const hotspot = document.createElement('div');
+  hotspot.id = 'hotspotId';
   hotspot.className = 'hotspot';
+  hotspot.ondragstart = e => {
+    dragStartX = e.clientX;
+    dragStartY = e.clientY;
+  };
+  hotspot.ondragend = e => {
+    const clientRect = e.currentTarget.getBoundingClientRect();
+    const x = clientRect.left + e.clientX - dragStartX;
+    const y = clientRect.top + e.clientY - dragStartY;
+    window.dropPosition = { x, y };
+    e.target.style.position = 'absolute';
+    e.target.style.top = y + 'px';
+    e.target.style.left = x + 'px';
+  };
+
+  const hotspotHeading = document.createElement('h5');
+  hotspotHeading.innerHTML = `Drag this hotspot to connect to another/ same room`
+
   hotspotWrapper.append(hotspotHeading);
   hotspotWrapper.append(hotspot);
 
