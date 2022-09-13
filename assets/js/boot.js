454e7107ea5e7e0509cca14c5b480f224e784b8e
Shubhankar Misra <shubhankar.misra@99acres.com>
2022-09-13 17:51:48 +0530

    saving major work

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -7,6 +7,22 @@ const { tourData = {}, } = window;
 const tourStartName = tourData.tourStart.tagName;
 const tourStartHotspot = tourData.metaInfo[tourStartName].hotspot;
 
+// At the boot time, currentCube will contain the value of entry point of the app. After that, as user will
+// navigate to other cubes, and currentCube value will be updated accordingly.
+window.currentCube = {};
+window.nextCube = {};
+
+const updateCurrentAndNextCubes = () => {
+  window.currentCube = { ...window.nextCube, };
+  const nextCubeName = (window.currentCube.hotspot || {}).name || '';
+  window.nextCube = {
+    ...window.nextCube,
+    name: (window.currentCube.hotspot || {}).name || '',
+    hotspot: (tourData.metaInfo[nextCubeName] || {}).hotspot || {},
+    images: (tourData.metaInfo[nextCubeName] || {}).images || [],
+  };
+};
+
 const handleHotspotClick = (event, { camera, scene, hotspot, }) => {
   let finalBox;
   const { point = {}, } = event.intersect || {};
@@ -18,35 +34,31 @@ const handleHotspotClick = (event, { camera, scene, hotspot, }) => {
     ease: Sine.easeInOut,
     onUpdate: () => {
       // Tour next stop images uploaded here
-      const tourStartName = tourData.tourStart.tagName
-      const nextStop = tourData.metaInfo[tourStartName].hotspot.name;
-      const images = tourData.metaInfo[nextStop].images;
-
+      const images = [...nextCube.images];
+      const hotspot = { ...nextCube.hotspot, };
       const progress = tweenOutside.progress();
-      if (progress >= 0.1) {
-        gsap.to('canvas', { ...progressUpdateHandler.start.parmas, });
-        scene.remove(hotspot);
-      }
-      if (progress >= 0.1 && progress <= 0.2) {
-        finalBox = createMesh({ images })
-      }
-      if (progress > 0.6) {
-        gsap.to('canvas', { delay: 0.3, opacity: 1, duration: 0.4, ease: Sine.easeInOut, },);
-      }
+
+      if (progress >= 0.1) gsap.to('canvas', { ...progressUpdateHandler.start.parmas, });
+      if (progress >= 0.1 && progress <= 0.2) finalBox = createMesh({ images })
+      if (progress > 0.6) gsap.to('canvas', { delay: 0.3, opacity: 1, duration: 0.4, ease: Sine.easeInOut, },);
     },
     onComplete: () => {
+      updateCurrentAndNextCubes();
       gsap.to('canvas', { ...progressUpdateHandler.finish.params });
       scene.add(finalBox);
     },
   });
 };
 
-function init() {
+function initApp() {
   // Create the basic elements for a 3-D scene.
   const elements = createElements();
   const camera = elements.camera;
   const scene = elements.scene;
   const renderer = elements.renderer;
+
+  // Cube data which is to be used for creating current view
+  const { hotspot: currentHotspot = {}, images: currentImages = [], } = window.currentCube || {};
 
   // create DOM eveents using camera and canvas;
   const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
@@ -55,13 +67,13 @@ function init() {
   const controls = initControls(camera);
 
   // creating the hotspot. You can pass in the custom geometry else it will use a default white sphere.
-  const hotspot = createHotspot({ scene, position: { x: tourStartHotspot.x, y: tourStartHotspot.y, z: 0, } });
+  const hotspot = createHotspot({ scene, position: { x: currentHotspot.x, y: currentHotspot.y, z: currentHotspot.z || 0, } });
 
   // Adding the click handler for hotspot
   domEvents.addEventListener(hotspot, 'click', event => handleHotspotClick(event, { camera, scene, hotspot, }));
 
   // Tour Start images uploaded here
-  const box = createMesh({ images: (((window.tourData || {}).metaInfo || {})[tourStartName] || {}).images || [], });
+  const box = createMesh({ images: currentImages, });
 
   scene.add(hotspot);
   scene.add(box);
@@ -72,4 +84,24 @@ function init() {
   animate();
 };
 
-init();
+function bootApp() {
+  // for new renders follow process: 
+  // 1- Update current and next cube window variables
+  // 2- call initApp
+  window.currentCube = {
+    ...(window.currentCube || {}),
+    name: ((window.tourData || {}).tourStart || {}).tagName || '',
+    hotspot: tourStartHotspot,
+    images: (((window.tourData || {}).metaInfo || {})[tourStartName] || {}).images || [],
+  };
+  const nextCubeName = (window.currentCube.hotspot || {}).name || '';
+  window.nextCube = {
+    ...window.nextCube,
+    name: window.currentCube.hotspot.name,
+    hotspot: (tourData.metaInfo[nextCubeName] || {}).hotspot || {},
+    images: (tourData.metaInfo[nextCubeName] || {}).images || [],
+  };
+  initApp();
+};
+
+bootApp();
